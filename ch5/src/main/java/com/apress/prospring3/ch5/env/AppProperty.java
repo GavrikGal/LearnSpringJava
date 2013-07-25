90c9c00cc75c6f148376ccd9c3c6b5d323c7a7cc
GavrikGal <gavrik.gal@gmail.com>
2013-07-25 15:26:55 +0200

    Добавлен еще пример с environment
    В примере приводится установка свойств окружения с использованием
    xml-файла конфигурации используется синтаксис типа: ${user.home}

---
+++ ch5/src/main/java/com/apress/prospring3/ch5/env/AppProperty.java
@@ -0,0 +1,20 @@
+package com.apress.prospring3.ch5.env;
+
+public class AppProperty {
+	
+	private String applicationHome;
+	private String userHome;
+	
+	public String getApplicationHome() {
+		return applicationHome;
+	}
+	public void setApplicationHome(String applicationHome) {
+		this.applicationHome = applicationHome;
+	}
+	public String getUserHome() {
+		return userHome;
+	}
+	public void setUserHome(String userHome) {
+		this.userHome = userHome;
+	}
+}
