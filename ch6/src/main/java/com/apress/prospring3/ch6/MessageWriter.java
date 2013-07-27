6e142e8f953c349b761062392405e44da1fc1228
GavrikGal <gavrik.gal@gmail.com>
2013-07-28 11:27:46 +1200

    AOP, пример Hello World.
    Рассмотрен простой пример с применением АОП.

---
+++ ch6/src/main/java/com/apress/prospring3/ch6/MessageWriter.java
@@ -0,0 +1,13 @@
+package com.apress.prospring3.ch6;
+
+public class MessageWriter {
+	
+	public void writeMessage() {
+		System.out.print("World");
+	}
+	
+	public void secondMessage() {
+		System.out.print("second message");
+	}
+
+}
