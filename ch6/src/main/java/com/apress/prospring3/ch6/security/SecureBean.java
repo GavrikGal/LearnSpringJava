28e6a3fa3cfd27404908ec3203b978f7460a0412
GavrikGal <gavrik.gal@gmail.com>
2013-07-29 11:57:08 +0200

    AOP, Совет "перед"
    Разобраны примеры создания приложений с использованием совета перед
    вызываемыми методами

---
+++ ch6/src/main/java/com/apress/prospring3/ch6/security/SecureBean.java
@@ -0,0 +1,9 @@
+package com.apress.prospring3.ch6.security;
+
+public class SecureBean {
+	
+	public void writeSecureMessage() {
+		System.out.println("Обама не следи за мной!!"
+				+ " Ничего ты не получишь!");
+	}
+}
