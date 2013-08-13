de8a49d070d51d3d8c60a239b10eef9ddbf6f18e
GavrikGal <gavrik.gal@gmail.com>
2013-08-13 11:38:42 +0200

    AOP, введения
    Рассмотрен пример "введения" для проверки изменения полей объектов.

---
+++ ch7/src/main/java/com/apress/prospring3/ch7/introductions/IsModifiedAdvisor.java
@@ -0,0 +1,10 @@
+package com.apress.prospring3.ch7.introductions;
+
+import org.springframework.aop.support.DefaultIntroductionAdvisor;
+
+public class IsModifiedAdvisor extends DefaultIntroductionAdvisor {
+
+	public IsModifiedAdvisor() {
+		super (new IsModifiedMixin());
+	}
+}
