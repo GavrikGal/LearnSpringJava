502147ad6b23aea3a65a66abe7a5a6eeace2498b
GavrikGal <gavrik.gal@gmail.com>
2013-10-08 16:14:57 +0300

    ch14 end

---
+++ ch14/src/main/java/com/apress/prospring3/ch14/pe/PropEditorExample.java
@@ -0,0 +1,24 @@
+package com.apress.prospring3.ch14.pe;
+
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+import com.apress.prospring3.ch14.domain.Contact;
+
+public class PropEditorExample {
+
+	public static void main(String[] args) {
+		
+		GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:prop-editor-app-context.xml");
+		ctx.refresh();
+		
+		Contact clarence = ctx.getBean("clarence", Contact.class);
+		System.out.println("Clarence info: " + clarence);
+		
+		Contact myContact = ctx.getBean("myContact", Contact.class);
+		System.out.println("My contact info: " + myContact);
+		
+		ctx.close();
+	}
+
+}
